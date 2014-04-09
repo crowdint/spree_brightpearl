b5d55bb44bedad0de41b032321f2d2deb89839e7
Mumo Carlos <mumo.crls@gmail.com>
2014-04-09 12:26:14 -0500

    [#68657108] Create Contact
    * Add spec and vcr for contact
    * Add spec and vcr for postal address

---
+++ spec/models/spree/bp_postal_address_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Spree::BpPostalAddress do
+
+  before do
+    Spree::Config[:brightpearl_email] = 'mumo.crls@gmail.com'
+    Spree::Config[:brightpearl_id] = 'mumoc'
+    Spree::Config[:brightpearl_password] = '220502Camubrightpearl'
+  end
+
+  describe '#save' do
+    let(:response) do
+      VCR.use_cassette 'bp/postal_address' do
+        subject.save
+      end
+    end
+
+    it 'sets the brightpearl id on user' do
+      expect(response).to eq({ id: 155 })
+    end
+  end
+end
+
+
