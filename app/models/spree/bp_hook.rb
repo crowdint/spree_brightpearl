4ce677b3a9edf6180d86ec7296332a24bdd4ea22
Steven Barragan <me@steven.mx>
2014-04-10 10:26:38 -0500

    Refactor webhook url

--- app/models/spree/bp_hook.rb
+++ app/models/spree/bp_hook.rb
@@ -1,7 +1,9 @@
 module Spree
   class BpHook < Brightpearl
+    include Rails.application.routes.url_helpers
+
     def set_products
-      Nacre::API::Hooks.product_resource('http://' + Spree::Config.site_url + '/bp_products')
+      Nacre::API::Hooks.product_resource(Spree.railtie_routes_url_helpers.bp_products_url( host: Spree::Config.site_url ))
     end
   end
 end
