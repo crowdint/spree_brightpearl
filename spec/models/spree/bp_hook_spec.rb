a40ee0efe1af9497e3cc4290571c661e5d8ab159
Steven Barragan <me@steven.mx>
2014-04-10 10:29:28 -0500

    Fix small typo

--- spec/models/spree/bp_hook_spec.rb
+++ spec/models/spree/bp_hook_spec.rb
@@ -21,7 +21,7 @@ describe Spree::BpHook do
       expect(@all.second['subscribeTo']).to eq 'product.modified'
     end
 
-    # Pendind due to brightpearl bug
+    # Pending due to brightpearl bug
     # https://www.brightpearl.com/community/forums/product-support-core-platform/webhook-http-method-delete
     xit 'sets product.destroyed webhook' do
       expect(@all.third['subscribeTo']).to eq 'product.destroyed'
