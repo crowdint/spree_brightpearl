d38738a99916b97321ab4039ae74e20d4640f677
Steven Barragan <me@steven.mx>
2014-04-10 10:49:26 -0500

    Avoid using instance variables, use 'let' instead

--- spec/models/spree/bp_hook_spec.rb
+++ spec/models/spree/bp_hook_spec.rb
@@ -6,25 +6,28 @@ describe Spree::BpHook do
       Spree::Config[:brightpearl_email] = 'steven@crowdint.com'
       Spree::Config[:brightpearl_id] = 'crowdint'
       Spree::Config[:brightpearl_password] = 'Nagarrab8'
+    end
 
+    let(:all) do
       VCR.use_cassette 'bp/product_webhooks' do
         subject.set_products
-        @all = Nacre::API::Hooks.all
+
+        Nacre::API::Hooks.all
       end
     end
 
     it 'sets product.created webhook' do
-      expect(@all.first['subscribeTo']).to eq 'product.created'
+      expect(all.first['subscribeTo']).to eq 'product.created'
     end
 
     it 'sets product.modified webhook' do
-      expect(@all.second['subscribeTo']).to eq 'product.modified'
+      expect(all.second['subscribeTo']).to eq 'product.modified'
     end
 
     # Pending due to brightpearl bug
     # https://www.brightpearl.com/community/forums/product-support-core-platform/webhook-http-method-delete
     xit 'sets product.destroyed webhook' do
-      expect(@all.third['subscribeTo']).to eq 'product.destroyed'
+      expect(all.third['subscribeTo']).to eq 'product.destroyed'
     end
   end
 end
