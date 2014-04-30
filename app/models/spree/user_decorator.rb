6c3d183b1bdc93f64c1153635355805ca3dfecb3
Mumo Carlos <mumo.crls@gmail.com>
2014-04-30 18:10:38 -0500

    [#68657108] Create Contact
    * Catch exception when creating users

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -3,16 +3,25 @@ module Spree
     before_create :create_brightpearl_resources
 
     def create_brightpearl_resources
-      bp_address = create_brightpearl_address
-      create_brightpearl_contact(bp_address[:id]) if bp_address
+      begin
+        bp_address = create_brightpearl_address
+        create_brightpearl_contact(bp_address[:id]) if bp_address
+      rescue Exception => e
+        Rails.logger.info "Brifhtpearl Error: #{e.message}"
+      end
     end
 
     def create_brightpearl_address
+      binding.pry
       Spree::BpPostalAddress.new.save
     end
 
     def create_brightpearl_contact(bp_address_id)
-      Spree::BpContact.new(self, bp_address_id).save
+      begin
+        Spree::BpContact.new(self, bp_address_id).save
+      rescue Exception => e
+        Rails.logger.info "Brigthpearl Error: #{e.message}"
+      end
     end
 
   end
