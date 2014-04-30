ea0b60090ac2af4368991eb4365b722b1ebb7936
Mumo Carlos <mumo.crls@gmail.com>
2014-04-30 18:19:00 -0500

    [#68657108] Create Contact
    * Removes binding.pry
    * Refactor begin rescue block to follow conventions

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -3,25 +3,20 @@ module Spree
     before_create :create_brightpearl_resources
 
     def create_brightpearl_resources
-      begin
-        bp_address = create_brightpearl_address
-        create_brightpearl_contact(bp_address[:id]) if bp_address
-      rescue Exception => e
-        Rails.logger.info "Brifhtpearl Error: #{e.message}"
-      end
+      bp_address = create_brightpearl_address
+      create_brightpearl_contact(bp_address[:id]) if bp_address
+    rescue Exception => e
+      Rails.logger.info "Brifhtpearl Error: #{e.message}"
     end
 
     def create_brightpearl_address
-      binding.pry
       Spree::BpPostalAddress.new.save
     end
 
     def create_brightpearl_contact(bp_address_id)
-      begin
-        Spree::BpContact.new(self, bp_address_id).save
-      rescue Exception => e
-        Rails.logger.info "Brigthpearl Error: #{e.message}"
-      end
+      Spree::BpContact.new(self, bp_address_id).save
+    rescue Exception => e
+      Rails.logger.info "Brigthpearl Error: #{e.message}"
     end
 
   end
