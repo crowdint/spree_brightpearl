42da3c85c33b91698e49c82fd7359113c28c5c17
Steven Barragán <me@steven.mx>
2014-06-30 18:16:35 -0500

    Fix failing test

--- spec/models/spree/payment_spec.rb
+++ spec/models/spree/payment_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe Spree::Payment do
   let(:subject) { create :payment }
+
+  before do
+    subject.order.update_attribute :brightpearl_id, 10
+  end
+
   describe '#save_to_brightpearl' do
     it 'adds one job' do
       expect{ subject.complete }.
