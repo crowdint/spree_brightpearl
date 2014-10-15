6643134a0accde63d426e3a5196661d11463d79b
Jaime Victoria <jaime.victoria@crowdint.com>
2014-10-15 13:52:01 -0500

    Adds state and city to create brightpearl address

--- app/models/spree/brightpearl/postal_address.rb
+++ app/models/spree/brightpearl/postal_address.rb
@@ -16,8 +16,19 @@ module Spree
       def bp_fields
         {
           addressLine1: @spree_address.try(:address1) || 'FootCardigan Address',
-          postalCode: @spree_address.try(:zipcode) || '115-243'
+          addressLine3: @spree_address.try(:city) || 'FootCardigan City',
+          addressLine4: state.try(:name) || 'FootCardigan State',
+          postalCode: @spree_address.try(:zipcode) || '115-243',
+          countryIsoCode: country.try(:iso3) || ''
         }
+      end
+
+      def country
+        @country ||= @spree_address.country if @spree_address
+      end
+
+      def state
+        @state ||= @spree_address.state if @spree_address
       end
     end
   end
